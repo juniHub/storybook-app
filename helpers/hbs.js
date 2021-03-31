f3fe3abcc961da804a7941e715230b14fcaad6b5
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2021-03-31 05:55:57 -0700

    some changes in UI

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -21,7 +21,7 @@ module.exports = {
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
-        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab indigo lighten-1"><i class="fas fa-edit fa-small"></i></a>`;
+        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab"><i class="fas fa-edit fa-small"></i></a>`;
       } else {
         return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
       }
