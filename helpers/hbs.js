85bf08e3a61d25a5ec4777c4b541c02b5fc4a6f2
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2021-05-04 06:32:44 -0700

    some changes in ui

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -46,8 +46,6 @@ module.exports = {
       );
   },
 
-  
-
   condition: function ( v1, operator, v2, options )
   {
 
@@ -73,6 +71,22 @@ module.exports = {
         return ( v1 && v2 ) ? options.fn( this ) : options.inverse( this );
       case '||':
         return ( v1 || v2 ) ? options.fn( this ) : options.inverse( this );
+      default:
+        return options.inverse( this );
+    }
+  },
+
+  statusCondition: function ( v1, operator, v2, options )
+  {
+
+    switch ( operator )
+    {
+      case '==':
+        return ( v1 == v2 ) ? options.fn( this ) : options.inverse( this );
+     
+      case '!=':
+        return ( v1 != v2 ) ? options.fn( this ) : options.inverse( this );
+    
       default:
         return options.inverse( this );
     }
